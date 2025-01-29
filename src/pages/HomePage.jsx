988b6c7de3b6404a729c84a07d7c5f299a306b05
AgusPaez <agus.123.paez@gmail.com>
2025-01-28 23:57:43 -0300

    feat: create Home, Footer, and Navbar components
    - Created basic structure for Home, Footer, and Navbar components
    - Components are empty, without functionality or HTML content

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1>Home Page</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
