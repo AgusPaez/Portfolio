fa91060031baf71af60b508160568d5f1e9252cc
AgusPaez <agus.123.paez@gmail.com>
2025-02-24 21:12:04 -0300

    import ThemeSwitcher

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Button from "./Button";
 import { motion } from "framer-motion";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -43,12 +44,12 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-[#f0f0f0bb] border-b-[1px] sticky top-0 z-50 w-full backdrop-blur-[19px]">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-10 lg:pr-20">
-        <div className="flex justify-between h-[90px]">
-          <div className="relative">
-            <div className="absolute left-23 top-8 px-2">
-              <a className="w-28 text-black text-3xl relative text-center cursor-pointer hover:text-gray-900 py-4 pb-1 text-md hover:tracking-widest font-medium link-hover-underline transition-all duration-700">
+    <nav className="bg-[#f0f0f0bb] dark:bg-[#18181893] border-b-[1px] dark:border-b-[#ffffff15] dark:backdrop-blur-[19px] sticky top-0 z-50 w-full  ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-10 lg:pr-20 ">
+        <div className="flex justify-between h-[90px] ">
+          <div className="relative dark:">
+            <div className="absolute left-23 top-8 px-2 ">
+              <a className="w-28 text-black dark:text-[#f0f0f0f1] text-[26px] opacity-0 md:opacity-100 md:text-3xl relative text-center cursor-pointer hover:text-gray-900 py-4 pb-1 text-md hover:tracking-widest font-medium link-hover-underline transition-all duration-700">
                 AP
               </a>
             </div>
@@ -57,21 +58,23 @@ const Navbar = () => {
               animate={{ opacity: 1, scale: 1, rotate: 9 }}
               transition={{ duration: 0.5 }}
               whileHover={{ scale: 1.1, rotate: 16 }}
-              className="bg-[#444444b6] h-12 w-12 rounded-[10px] absolute left-8 top-5"
+              className="bg-[#444444b6] dark:bg-[#bbbbbbb6] h-12 w-12 rounded-[10px] absolute md:left-8 left-4 md:top-5 top-6"
             >
               <motion.div
                 initial={{ opacity: 0, y: -20, rotate: 82 }}
                 animate={{ opacity: 1, y: 0, rotate: 82 }}
                 transition={{ duration: 0.5, delay: 0.2 }}
                 whileHover={{ scale: 1.1, rotate: 87 }}
-                className="bg-[#292929e0] h-[37px] w-[37px] rounded-[10px] mx-auto mt-[6px]"
+                className="bg-[#292929e0] dark:bg-[#dfdfdfb6] h-[37px] w-[37px] rounded-[10px] mx-auto mt-[6px]"
               >
                 <motion.div
                   initial={{ rotate: 90 }}
                   transition={{ duration: 0.5, delay: 0.4 }}
                   className="text-right mt-1"
                 >
-                  <a className="text-right">{"</>"}</a>
+                  <a className="text-right dark:text-black dark:font-extrabold">
+                    {"</>"}
+                  </a>
                 </motion.div>
               </motion.div>
             </motion.div>
@@ -109,9 +112,13 @@ const Navbar = () => {
                 time={0.9}
                 active={activeSection === "contacto"}
               />
+              <ThemeSwitcher />
             </div>
           ) : (
-            <div className="-mr-2 flex md:hidden">
+            <div className="-mr-2 flex justify-spac md:hidden ">
+              <div className="my-auto w-full pr-22">
+                <ThemeSwitcher />
+              </div>
               <button
                 onClick={toggleMenu}
                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 focus:outline-none "
